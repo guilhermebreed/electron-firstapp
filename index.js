4f766a53c3c85270cc9fb0770a28a66d7d28994a
Guilherme Silva <guilherme.bralves@hotmail.com>
2018-08-28 17:05:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron')
+
+app.on('ready', () => {
+   let mainWindow = new BrowserWindow({
+       titleBarStyle: 'hidden-inset'
+   })
+
+   mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+   mainWindow.webContents.on('will-navigate', (e, url) => {
+       e.preventDefault();
+       
+       mainWindow.webContents.send('open-file', url);
+   })
+})
